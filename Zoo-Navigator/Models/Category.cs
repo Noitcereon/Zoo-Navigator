e8833162466542986451b5d1c2fe80a5d62c4753
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2019-12-03 09:36:52 +0100

    Added observer to MainViewModel.

--- Zoo-Navigator/Models/Category.cs
+++ Zoo-Navigator/Models/Category.cs
@@ -8,5 +8,6 @@ namespace Zoo_Navigator.Models
 {
     class Category
     {
+
     }
 }
--- Zoo-Navigator/ViewModels/MainViewModel.cs
+++ Zoo-Navigator/ViewModels/MainViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Zoo_Navigator.Annotations;
 
 namespace Zoo_Navigator.ViewModels
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
